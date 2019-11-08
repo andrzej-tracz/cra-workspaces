f76e146da4a5a765a90a6288c82ab85c03d716d5
Andrzej Tracz <andrzej.tracz7@gmail.com>
2019-11-08 20:36:47 +0100

    Updates

---
+++ precache-manifest.8d2918524047e563fd7aaf0f84a5ec8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392a0d2248560ec738201b5874a05ae5",
+    "url": "/cra-workspaces/index.html"
+  },
+  {
+    "revision": "a702c8d2463ca9aacb71",
+    "url": "/cra-workspaces/static/css/main.dcfe4c72.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/cra-workspaces/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "a702c8d2463ca9aacb71",
+    "url": "/cra-workspaces/static/js/main.153193f5.chunk.js"
+  },
+  {
+    "revision": "7e016eaa88e86e274a1a",
+    "url": "/cra-workspaces/static/js/runtime~main.18877d3a.js"
+  },
+  {
+    "revision": "2c813d88489d34f9b9f954e2fac67f5d",
+    "url": "/cra-workspaces/static/media/ReactLogo.2c813d88.svg"
+  },
+  {
+    "revision": "42730579f20fe80bb9c071bcadbcdd6a",
+    "url": "/cra-workspaces/static/media/YarnCat.42730579.svg"
+  }
+]);
